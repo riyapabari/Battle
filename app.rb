5ec7a232b8cee96535904eaacf90b927ecac1d10
riyapabari <riya.pabari@gmail.com>
2016-06-08 16:56:49 +0100

    Challenge 19: added sessions

--- app.rb
+++ app.rb
@@ -1,14 +1,26 @@
 require 'sinatra/base'
 
+
+
 class Battle < Sinatra::Base
+
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
     p params
-    @player1 = params[:player_one_name]
-    @player2 = params[:player_two_name]
+    session[:player_one_name] = params[:player_one_name]
+    session[:player_two_name] = params[:player_two_name]
+    redirect('/play')
+  end
+
+  get '/play' do
+    @player1 = session[:player_one_name]
+    @player2 = session[:player_two_name]
     erb(:play)
   end
 
