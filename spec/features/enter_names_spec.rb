00169cb986aa944076bec72f6f615d812039aee4
lomlo <michael.lo@live.co.uk>
2016-06-08 17:16:06 +0100

    step 20 complete, Player one can now see Player Twos hitpoints

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -10,3 +10,13 @@ feature 'players can fill-in names, submit and see' do
     expect(page).to have_content('Aquarious vs Pisces')
   end
 end
+
+feature "Player 1 can see Player 2's hitpoints" do
+  scenario "Player 1 sees Player 2's hitpoints" do
+    visit '/'
+    fill_in('player_one_name', with: 'Aquarious')
+    fill_in('player_two_name', with: 'Pisces')
+    click_button('Submit')
+    expect(page).to have_content("Pisces's Hit Points")
+  end
+end
